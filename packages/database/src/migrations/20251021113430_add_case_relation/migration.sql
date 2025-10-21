ee555088f1de3a5f611bc63ede9fae4e0fad2b59
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-10-21 12:34:46 +0100

    feat(portal): alter schema and migration

--- packages/database/src/migrations/20251021113430_add_case_relation/migration.sql
+++ packages/database/src/migrations/20251021113430_add_case_relation/migration.sql
@@ -4,6 +4,7 @@
   - The primary key for the `OneTimePassword` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to alter the column `id` on the `OneTimePassword` table. The data in that column could be lost. The data in that column will be cast from `NVarChar(1000)` to `UniqueIdentifier`.
   - Added the required column `caseId` to the `Document` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `isCertified` to the `Document` table without a default value. This is not possible if the table is not empty.
 
 */
 BEGIN TRY
@@ -11,7 +12,9 @@ BEGIN TRY
 BEGIN TRAN;
 
 -- AlterTable
-ALTER TABLE [dbo].[Document] ADD [caseId] UNIQUEIDENTIFIER NOT NULL;
+ALTER TABLE [dbo].[Document] ADD [caseId] UNIQUEIDENTIFIER NOT NULL,
+[isCertified] BIT NOT NULL,
+[uploadedDate] DATETIME2 NOT NULL CONSTRAINT [Document_uploadedDate_df] DEFAULT CURRENT_TIMESTAMP;
 
 -- AlterTable
 ALTER TABLE [dbo].[OneTimePassword] DROP CONSTRAINT [OneTimePassword_pkey];
