28d9f096657436d4a214edadf03784891df70df9
Prashant Pundir <165817128+prashantproX@users.noreply.github.com>
2025-10-21 15:40:44 +0100

    feat(portal): move CategoryStatus type to data-static file

--- apps/portal/src/app/views/home/controller.ts
+++ apps/portal/src/app/views/home/controller.ts
@@ -1,8 +1,6 @@
 import type { PortalService } from '#service';
 import type { AsyncRequestHandler } from '@pins/dco-portal-lib/util/async-handler.ts';
-import { DOCUMENT_CATEGORY } from '@pins/dco-portal-database/src/seed/data-static.ts';
-
-type CategoryStatus = 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED';
+import { DOCUMENT_CATEGORY, CategoryStatus } from '@pins/dco-portal-database/src/seed/data-static.ts';
 
 function getCategoryStatus(status: CategoryStatus): { text: string; classes: string } {
 	switch (status) {
