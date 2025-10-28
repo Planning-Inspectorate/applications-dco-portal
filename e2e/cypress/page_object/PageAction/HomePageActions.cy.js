b1a7519f37bb13275f990603888d5b71e461e06a
james.barnes <james.barnes@solirius.com>
2025-10-28 13:12:08 +0000

    test: create homepage locators and actions

--- e2e/cypress/page_object/PageAction/HomePageActions.cy.js
+++ e2e/cypress/page_object/PageAction/HomePageActions.cy.js
@@ -1,3 +1,23 @@
-export class HomePageActions {}
+import homePageLocators from '../PageLocators/homePageLocators.js';
+
+export class HomePageActions {
+	enterEmailAddress(email) {
+		homePageLocators.emailInput().clear().type(email);
+	}
+
+	enterApplicationNumber(applicationNumber) {
+		homePageLocators.applicationNumberInput().clear().type(applicationNumber);
+	}
+
+	submitLoginDetails() {
+		cy.get("[data-cy='button-save-and-continue']").click();
+	}
+
+	login(email, applicationNumber) {
+		this.enterEmailAddress(email);
+		this.enterApplicationNumber(applicationNumber);
+		this.submitLoginDetails();
+	}
+}
 
 export default new HomePageActions();
