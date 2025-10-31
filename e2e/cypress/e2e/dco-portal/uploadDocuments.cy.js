ecd2a76089d619437fd5a4869a40e9e5373a6da6
james.barnes <james.barnes@solirius.com>
2025-10-31 17:25:05 +0000

    test: e2e test for input validation

--- e2e/cypress/e2e/dco-portal/uploadDocuments.cy.js
+++ e2e/cypress/e2e/dco-portal/uploadDocuments.cy.js
@@ -5,7 +5,7 @@ import UploadDocumentsLocators from '../../page_object/PageLocators/uploadDocume
 import CommonLocators from '../../page_object/PageLocators/commonLocators.js';
 
 describe('1. Your documents', () => {
-	it('Application form related information journey', () => {
+	it('Application form related information standard journey', () => {
 		CommonActions.login();
 		cy.get('h1').should('be.visible').contains('Application reference number');
 		cy.get('h2').should('be.visible').contains('Your documents');
@@ -13,7 +13,7 @@ describe('1. Your documents', () => {
 		//open first task
 		UploadDocumentsActions.openTask(0);
 		cy.url().should('include', '/application-form-related-information');
-		cy.get('p').contains('This category contains').should('be.visible');
+		cy.get('p').contains('This category contains 0 document(s)').should('be.visible');
 
 		//document upload process
 		UploadDocumentsLocators.uploadDocumentsButton().click();
@@ -51,8 +51,55 @@ describe('1. Your documents', () => {
 		cy.get('.govuk-summary-list').children('div').eq(2).find('dd').contains('5').should('be.visible');
 		cy.get('.govuk-summary-list').children('div').eq(3).find('dd').contains('Yes').should('be.visible');
 
+		/*
+		Need to enforce the redirect and normal behaviour after submitting without persisting to the database. You could stub it?
+
 		cy.get('.govuk-button').contains('Confirm upload').click();
 		cy.get('input[name="applicationFormRelatedInformationIsCompleted"][value="yes"]').click();
 		cy.get('button[data-module="govuk-button"]').click();
+		*/
+	});
+	/*
+	TODO
+	chunk each part of the process into an action - make this and the last one more modular
+	*/
+	it('Application form related information standard journey', () => {
+		CommonActions.login();
+		cy.get('h1').should('be.visible').contains('Application reference number');
+		cy.get('h2').should('be.visible').contains('Your documents');
+
+		//open first task
+		UploadDocumentsActions.openTask(0);
+		cy.url().should('include', '/application-form-related-information');
+		cy.get('p').contains('This category contains 0 document(s)').should('be.visible');
+
+		//document upload process
+		UploadDocumentsLocators.uploadDocumentsButton().click();
+		cy.url().should('include', '/application-form-related-information/upload/document-type');
+		CommonLocators.saveAndContinueButton().click();
+		cy.get('.govuk-error-summary__title').should('be.visible').contains('There is a problem');
+		cy.get('a').contains('You must select an answer');
+		UploadDocumentsLocators.getDocumentTypeRadioButtonByValue('guide-to-the-application').check();
+		CommonLocators.saveAndContinueButton().click();
+
+		cy.url().should('include', '/application-form-related-information/upload/upload-documents');
+		CommonLocators.saveAndContinueButton().click();
+		cy.get('.govuk-error-summary__title').should('be.visible').contains('There is a problem');
+		cy.get('a').contains('Upload an attachment');
+		cy.get('input[type="file"]').selectFile('cypress/fixtures/uploadTest.pdf', { force: true });
+		CommonLocators.saveAndContinueButton().click();
+
+		cy.url().should('include', '/application-form-related-information/upload/regulation');
+		CommonLocators.saveAndContinueButton().click();
+		cy.get('.govuk-error-summary__title').should('be.visible').contains('There is a problem');
+		cy.get('a').contains('You must select an answer');
+		UploadDocumentsLocators.apfpRegulationTextbox().type('5');
+		cy.get('.autocomplete__option').should('be.visible').first().click();
+		CommonLocators.saveAndContinueButton().click();
+
+		cy.url().should('include', '/application-form-related-information/upload/document-certified');
+		CommonLocators.saveAndContinueButton().click();
+		cy.get('.govuk-error-summary__title').should('be.visible').contains('There is a problem');
+		cy.get('a').contains('You must select an answer');
 	});
 });
