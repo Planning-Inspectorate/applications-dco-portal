f928f2e8a20c0b233f1b4c29c1f2e32e68d9874d
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-11-28 08:50:37 +0000

    chore(database): use @prisma/adapter-mssql

--- apps/portal/src/app/config-types.d.ts
+++ apps/portal/src/app/config-types.d.ts
@@ -1,4 +1,3 @@
-import { Prisma } from '@pins/dco-portal-database/src/client';
 import { NotifyConfig } from '@pins/dco-portal-lib/govnotify/types';
 import { BlobStoreConfig } from '@pins/dco-portal-lib/blob-store/types';
 
@@ -8,7 +7,9 @@ interface Config {
 	cacheControl: {
 		maxAge: string;
 	};
-	database: Prisma.PrismaClientOptions;
+	database: {
+		datasourceUrl: string;
+	};
 	gitSha?: string;
 	govNotify: NotifyConfig;
 	httpPort: number;
