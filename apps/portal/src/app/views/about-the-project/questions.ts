abf2a7c9bfeaad58858c206c1639b6945d7742ec
james.barnes <james.barnes@solirius.com>
2025-12-01 11:21:02 +0000

    feat: implement basic about the project journey

---
+++ apps/portal/src/app/views/about-the-project/questions.ts
@@ -0,0 +1,35 @@
+// @ts-expect-error - due to not having @types
+import StringValidator from '@planning-inspectorate/dynamic-forms/src/validator/string-validator.js';
+// @ts-expect-error - due to not having @types
+import { createQuestions } from '@planning-inspectorate/dynamic-forms/src/questions/create-questions.js';
+// @ts-expect-error - due to not having @types
+import { questionClasses } from '@planning-inspectorate/dynamic-forms/src/questions/questions.js';
+// @ts-expect-error - due to not having @types
+import { COMPONENT_TYPES } from '@planning-inspectorate/dynamic-forms';
+import { CUSTOM_COMPONENT_CLASSES } from '@pins/dco-portal-lib/forms/custom-components/index.ts';
+
+export function getQuestions() {
+	const questions = {
+		description: {
+			type: COMPONENT_TYPES.TEXT_ENTRY,
+			title: 'Project Description',
+			pageTitle: 'Project Description',
+			question: 'Describe the Project',
+			fieldName: 'description',
+			url: 'description',
+			validators: [
+				new StringValidator({
+					maxLength: { maxLength: 2000 },
+					minLength: { minLength: 2 }
+				})
+			]
+		}
+	};
+
+	const classes = {
+		...questionClasses,
+		...CUSTOM_COMPONENT_CLASSES
+	};
+
+	return createQuestions(questions, classes, {});
+}
---
+++ apps/portal/src/app/views/about-the-project/save.ts
@@ -0,0 +1,76 @@
+import type { PortalService } from '#service';
+import type { AsyncRequestHandler } from '@pins/dco-portal-lib/util/async-handler.ts';
+// @ts-expect-error - due to not having @types
+import { clearDataFromSession } from '@planning-inspectorate/dynamic-forms/src/lib/session-answer-store.js';
+import { getAnswersFromRes } from '@pins/dco-portal-lib/util/answers.ts';
+
+export function buildSaveController({ db, logger }: PortalService, applicationSectionId: string): AsyncRequestHandler {
+	return async (req, res) => {
+		const answers = getAnswersFromRes(res);
+		console.log(answers);
+		try {
+			/* 
+            await db.$transaction(async ($tx) => {
+                const caseData = await $tx.case.findUnique({
+                    where: { reference: req.session?.caseReference },
+                    include: {
+                        ApplicantDetails: {
+                            include: {
+                                Address: true
+                            }
+                        }
+                    }
+                });
+
+                const [input, address] = [mapAnswersToInput(answers), mapAnswersToFullAddressInput(answers.address)];
+
+                if (caseData?.ApplicantDetails) {
+                    await $tx.contactDetails.update({
+                        where: { id: caseData?.ApplicantDetails.id },
+                        data: {
+                            ...input,
+                            Address: {
+                                upsert: {
+                                    update: address,
+                                    create: address
+                                }
+                            }
+                        }
+                    });
+                } else {
+                    await $tx.case.update({
+                        where: { reference: req.session.caseReference },
+                        data: {
+                            ApplicantDetails: {
+                                create: {
+                                    ...input,
+                                    Address: {
+                                        create: address
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+
+                if (
+                    (caseData as any)[`${kebabCaseToCamelCase(applicationSectionId)}StatusId`] !==
+                    DOCUMENT_CATEGORY_STATUS_ID.IN_PROGRESS
+                ) {
+                    await $tx.case.update({
+                        where: { reference: req.session.caseReference },
+                        data: { [`${kebabCaseToCamelCase(applicationSectionId)}StatusId`]: DOCUMENT_CATEGORY_STATUS_ID.IN_PROGRESS }
+                    });
+                }
+            });
+            */
+		} catch (error) {
+			logger.error({ error }, 'error saving applicant agent details data to database');
+			throw new Error('error saving applicant agent details journey');
+		}
+
+		clearDataFromSession({ req, journeyId: applicationSectionId });
+
+		res.redirect('/');
+	};
+}
