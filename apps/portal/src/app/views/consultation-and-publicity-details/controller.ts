ffbca1847970fb1e0e8a3ea85831a6a182fe15e4
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-12-01 11:49:59 +0000

    feat(portal): create consultationAndPublicityDetails journey

--- apps/portal/src/app/views/consultation-and-publicity-details/controller.ts
+++ apps/portal/src/app/views/consultation-and-publicity-details/controller.ts
@@ -1,8 +1,68 @@
 import type { AsyncRequestHandler } from '@pins/dco-portal-lib/util/async-handler.ts';
+import type { PortalService } from '#service';
+import { kebabCaseToCamelCase } from '@pins/dco-portal-lib/util/questions.ts';
+import {
+	DOCUMENT_CATEGORY_STATUS_ID,
+	DOCUMENT_SUB_CATEGORY_ID
+} from '@pins/dco-portal-database/src/seed/data-static.ts';
+import type { Request, Response } from 'express';
+import { notFoundHandler } from '@pins/dco-portal-lib/middleware/errors.ts';
+import { getSupportingEvidenceIds } from '../supporting-evidence/util.ts';
+import type { PrismaClient } from '@pins/dco-portal-database/src/client';
 
-export function buildConsultationAndPublicityHomePage(): AsyncRequestHandler {
+export function buildConsultationAndPublicityHomePage(
+	{ db }: PortalService,
+	applicationSectionId: string
+): AsyncRequestHandler {
 	return async (req, res) => {
-		//TODO: update the section segment of the url to reflect created journey
-		res.redirect(`${req.baseUrl}/consultation-report`);
+		const caseData = await db.case.findUnique({
+			where: { reference: req.session?.caseReference }
+		});
+
+		if (
+			(caseData as any)[`${kebabCaseToCamelCase(applicationSectionId)}StatusId`] !==
+			DOCUMENT_CATEGORY_STATUS_ID.NOT_STARTED
+		) {
+			await populateForm(req, res, db, applicationSectionId);
+		}
+
+		res.redirect(`${req.baseUrl}/details/consultation-report`);
+	};
+}
+
+async function populateForm(req: Request, res: Response, db: PrismaClient, applicationSectionId: string) {
+	const consultationAndPublicityDetailsDocumentIds = [
+		DOCUMENT_SUB_CATEGORY_ID.CONSULTATION_REPORT,
+		DOCUMENT_SUB_CATEGORY_ID.CONSULTATION_REPORT_APPENDICES
+	];
+
+	const caseData = await db.case.findUnique({
+		where: { reference: req.session?.caseReference },
+		include: {
+			SupportingEvidence: {
+				where: {
+					SubCategory: {
+						id: { in: consultationAndPublicityDetailsDocumentIds }
+					}
+				}
+			}
+		}
+	});
+
+	if (!caseData) {
+		return notFoundHandler(req, res);
+	}
+
+	const forms = req.session.forms || (req.session.forms = {});
+
+	forms[applicationSectionId] = {
+		consultationReport: getSupportingEvidenceIds(
+			caseData.SupportingEvidence,
+			DOCUMENT_SUB_CATEGORY_ID.CONSULTATION_REPORT
+		),
+		consultationReportAppendices: getSupportingEvidenceIds(
+			caseData.SupportingEvidence,
+			DOCUMENT_SUB_CATEGORY_ID.CONSULTATION_REPORT_APPENDICES
+		)
 	};
 }
--- apps/portal/src/app/views/consultation-and-publicity-details/index.ts
+++ apps/portal/src/app/views/consultation-and-publicity-details/index.ts
@@ -1,23 +1,57 @@
-import { Router as createRouter } from 'express';
+import { type Handler, type Request, Router as createRouter } from 'express';
 import type { IRouter } from 'express';
 
 import type { PortalService } from '#service';
 import { asyncHandler } from '@pins/dco-portal-lib/util/async-handler.ts';
 import { buildConsultationAndPublicityHomePage } from './controller.ts';
-import { buildIsTaskCompleted } from '../util.ts';
+// @ts-expect-error - due to not having @types
+import { buildGetJourney } from '@planning-inspectorate/dynamic-forms/src/middleware/build-get-journey.js';
+import {
+	buildGetJourneyResponseFromSession,
+	saveDataToSession
+	// @ts-expect-error - due to not having @types
+} from '@planning-inspectorate/dynamic-forms/src/lib/session-answer-store.js';
+// @ts-expect-error - due to not having @types
+import { buildSave, list, question } from '@planning-inspectorate/dynamic-forms/src/controller.js';
+// @ts-expect-error - due to not having @types
+import validate from '@planning-inspectorate/dynamic-forms/src/validator/validator.js';
+// @ts-expect-error - due to not having @types
+import { validationErrorHandler } from '@planning-inspectorate/dynamic-forms/src/validator/validation-error-handler.js';
+import { getApplicationSectionDisplayName } from '../util.ts';
+import { selectDocumentQuestionMiddleware } from '../middleware/select-document-middleware.ts';
+import { getQuestions } from './questions.ts';
+import { createJourney } from './journey.ts';
+import { buildSaveController } from './save.ts';
 
 export function createRoutes(service: PortalService, applicationSectionId: string): IRouter {
 	const router = createRouter({ mergeParams: true });
-
-	const consultationAndPublicityHomePage = buildConsultationAndPublicityHomePage();
-	const isConsultationAndPublicityCompleted = buildIsTaskCompleted(
-		service,
-		applicationSectionId,
-		buildConsultationAndPublicityHomePage
+	const questions = getQuestions();
+	const getJourney = buildGetJourney((req: Request, journeyResponse: Handler) =>
+		createJourney(applicationSectionId, questions, journeyResponse, req)
 	);
+	const getJourneyResponse = buildGetJourneyResponseFromSession(applicationSectionId);
+
+	const consultationAndPublicityHomePage = buildConsultationAndPublicityHomePage(service, applicationSectionId);
+	const saveController = buildSaveController(service, applicationSectionId);
+
+	const selectDocumentQuestion = selectDocumentQuestionMiddleware(service);
 
 	router.get('/', asyncHandler(consultationAndPublicityHomePage));
-	router.post('/', isConsultationAndPublicityCompleted);
+
+	router.get('/:section/:question', getJourneyResponse, getJourney, selectDocumentQuestion, question);
+	router.post(
+		'/:section/:question',
+		getJourneyResponse,
+		getJourney,
+		validate,
+		validationErrorHandler,
+		buildSave(saveDataToSession)
+	);
+
+	router.get('/check-your-answers', getJourneyResponse, getJourney, (req, res) =>
+		list(req, res, getApplicationSectionDisplayName(applicationSectionId), {})
+	);
+	router.post('/check-your-answers', getJourneyResponse, getJourney, asyncHandler(saveController));
 
 	return router;
 }
