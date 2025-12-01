0e62658ec9947be9d766de9076bd6c3a9f557409
james.barnes <james.barnes@solirius.com>
2025-12-01 11:21:02 +0000

    feat: implement basic about the project journey

--- apps/portal/src/app/views/about-the-project/controller.ts
+++ apps/portal/src/app/views/about-the-project/controller.ts
@@ -3,6 +3,6 @@ import type { AsyncRequestHandler } from '@pins/dco-portal-lib/util/async-handle
 export function buildAboutTheProjectHomePage(): AsyncRequestHandler {
 	return async (req, res) => {
 		//TODO: update the section segment of the url to reflect created journey
-		res.redirect(`${req.baseUrl}/why-project-requires-development-consent`);
+		res.redirect(`${req.baseUrl}/about/description`);
 	};
 }
