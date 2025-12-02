aaa67eea8ea40128750a1c424a9ccc39dc5fd73f
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-12-02 11:42:20 +0000

    feat(portal): add guard to prevent file deletion if used as supporting evidence

--- apps/portal/src/app/views/file-upload/controller.test.ts
+++ apps/portal/src/app/views/file-upload/controller.test.ts
@@ -74,7 +74,7 @@ describe('file upload controllers', () => {
 				documents: [
 					[
 						{
-							html: '<a class="govuk-link" href="/draft-dco/document/download/doc-id-1" target="_blank" rel="noreferrer">test.pdf</a>'
+							html: '<a class="govuk-link govuk-link--no-visited-state" href="/draft-dco/document/download/doc-id-1" target="_blank" rel="noreferrer">test.pdf</a>'
 						},
 						{
 							text: 'Draft development consent order'
@@ -89,7 +89,7 @@ describe('file upload controllers', () => {
 							text: '30/01/2025 00:00'
 						},
 						{
-							html: '<a class="govuk-link" href="/draft-dco/document/delete/doc-id-1">Remove</a>'
+							html: '<a class="govuk-link govuk-link--no-visited-state" href="/draft-dco/document/delete/doc-id-1">Remove</a>'
 						}
 					]
 				],
@@ -186,6 +186,9 @@ describe('file upload controllers', () => {
 					})),
 					count: mock.fn(() => 0),
 					delete: mock.fn()
+				},
+				supportingEvidence: {
+					findUnique: mock.fn()
 				}
 			};
 			const mockBlob = {
@@ -240,6 +243,130 @@ describe('file upload controllers', () => {
 				}
 			});
 		});
+		it('should delete document from blob store and update db to reflect', async (ctx) => {
+			const now = new Date('2025-01-30T00:00:07.000Z');
+			ctx.mock.timers.enable({ apis: ['Date'], now });
+
+			const mockDb = {
+				$transaction: mock.fn((fn) => fn(mockDb)),
+				case: {
+					findUnique: mock.fn(() => ({
+						reference: 'case-ref-1',
+						draftDcoStatusId: 'in-progress',
+						Documents: [
+							{
+								id: 'doc-id-1',
+								fileName: 'test.pdf',
+								uploadedDate: Date.now(),
+								isCertified: true,
+								apfpRegulationId: '5-1',
+								ApfpRegulation: {
+									id: '5-1',
+									displayName: '5(1)'
+								},
+								subCategoryId: 'draft-development-consent-order',
+								SubCategory: {
+									id: 'draft-development-consent-order',
+									displayName: 'Draft development consent order',
+									Category: {
+										id: 'draft-dco',
+										displayName: 'Draft DCO'
+									}
+								}
+							}
+						]
+					}))
+				},
+				document: {
+					findUnique: mock.fn(() => ({
+						blobName: '/case-id-1/draft-dco/test.pdf'
+					}))
+				},
+				supportingEvidence: {
+					findUnique: mock.fn(() => ({
+						id: '431e803f-9e12-4140-8671-24f7be807c47',
+						caseId: '18be8caa-9176-4d69-8f1a-d3659fed89e8',
+						documentId: 'f4194748-f9a1-4ee3-a8aa-87172c42f480',
+						subCategoryId: 'draft-dco'
+					}))
+				},
+				documentCategory: {
+					findUnique: mock.fn(() => ({
+						id: DOCUMENT_CATEGORY_ID.DRAFT_DCO,
+						displayName: 'Draft DCO'
+					}))
+				}
+			};
+			const mockBlob = {
+				deleteBlobIfExists: mock.fn()
+			};
+			const mockReq = {
+				baseUrl: '/draft-dco',
+				params: {
+					documentId: 'doc-id-1'
+				},
+				session: {
+					caseReference: 'EN123456'
+				}
+			};
+			const mockRes = {
+				render: mock.fn()
+			};
+
+			const controller = buildDeleteDocumentAndSaveController(
+				{
+					db: mockDb,
+					blobStore: mockBlob,
+					logger: mockLogger()
+				},
+				'draft-dco'
+			);
+			await controller(mockReq, mockRes);
+
+			assert.strictEqual(mockRes.render.mock.callCount(), 1);
+			assert.strictEqual(mockRes.render.mock.calls[0].arguments[0], 'views/file-upload/view.njk');
+			assert.deepStrictEqual(mockRes.render.mock.calls[0].arguments[1], {
+				pageTitle: 'Draft DCO',
+				documentCategory: 'draftDco',
+				documents: [
+					[
+						{
+							html: '<a class="govuk-link govuk-link--no-visited-state" href="/draft-dco/document/download/doc-id-1" target="_blank" rel="noreferrer">test.pdf</a>'
+						},
+						{
+							text: 'Draft development consent order'
+						},
+						{
+							text: '5(1)'
+						},
+						{
+							text: 'Certified'
+						},
+						{
+							text: '30/01/2025 00:00'
+						},
+						{
+							html: '<a class="govuk-link govuk-link--no-visited-state" href="/draft-dco/document/delete/doc-id-1">Remove</a>'
+						}
+					]
+				],
+				showUploadButton: true,
+				uploadButtonUrl: '/draft-dco/upload/document-type',
+				backLinkUrl: '/',
+				isCompletedValue: 'no',
+				errors: {
+					draftDcoDocumentTable: {
+						msg: 'You cannot delete a document that is being used as supporting evidence in the application form'
+					}
+				},
+				errorSummary: [
+					{
+						text: 'You cannot delete a document that is being used as supporting evidence in the application form',
+						href: '#draftDcoDocumentTable'
+					}
+				]
+			});
+		});
 		it('should throw error if error encountered during blob store deletion', async (ctx) => {
 			const mockDb = {
 				$transaction: mock.fn((fn) => fn(mockDb)),
@@ -247,6 +374,9 @@ describe('file upload controllers', () => {
 					findUnique: mock.fn(() => ({
 						blobName: '/case-id-1/draft-dco/test.pdf'
 					}))
+				},
+				supportingEvidence: {
+					findUnique: mock.fn()
 				}
 			};
 			const mockBlob = {
@@ -282,6 +412,9 @@ describe('file upload controllers', () => {
 					delete: mock.fn(() => {
 						throw new Prisma.PrismaClientKnownRequestError('Error', { code: 'E1' });
 					})
+				},
+				supportingEvidence: {
+					findUnique: mock.fn()
 				}
 			};
 			const mockBlob = {
