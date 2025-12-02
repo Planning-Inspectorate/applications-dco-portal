d3be29e5f8d727d4aa600f60d9d3449b510a0fa8
james.barnes <james.barnes@solirius.com>
2025-12-02 17:01:38 +0000

    feat: implement single and linear grid reference questions

--- apps/portal/src/app/views/about-the-project/questions.ts
+++ apps/portal/src/app/views/about-the-project/questions.ts
@@ -1,12 +1,16 @@
 // @ts-expect-error - due to not having @types
 import StringValidator from '@planning-inspectorate/dynamic-forms/src/validator/string-validator.js';
 // @ts-expect-error - due to not having @types
+import RequiredValidator from '@planning-inspectorate/dynamic-forms/src/validator/required-validator.js';
+// @ts-expect-error - due to not having @types
+import MultiFieldInputValidator from '@planning-inspectorate/dynamic-forms/src/validator/multi-field-input-validator.js';
+// @ts-expect-error - due to not having @types
 import { createQuestions } from '@planning-inspectorate/dynamic-forms/src/questions/create-questions.js';
 // @ts-expect-error - due to not having @types
 import { questionClasses } from '@planning-inspectorate/dynamic-forms/src/questions/questions.js';
 // @ts-expect-error - due to not having @types
 import { COMPONENT_TYPES } from '@planning-inspectorate/dynamic-forms';
-import { CUSTOM_COMPONENT_CLASSES } from '@pins/dco-portal-lib/forms/custom-components/index.ts';
+import { CUSTOM_COMPONENTS, CUSTOM_COMPONENT_CLASSES } from '@pins/dco-portal-lib/forms/custom-components/index.ts';
 
 export function getQuestions() {
 	const questions = {
@@ -36,6 +40,136 @@ export function getQuestions() {
 					maxLength: { maxLength: 2000 },
 					minLength: { minLength: 2 }
 				})
+			]
+		},
+		locationDescription: {
+			type: COMPONENT_TYPES.TEXT_ENTRY,
+			title: 'Project Location Description',
+			pageTitle: 'Project Location Description',
+			question: 'Describe the location or route of the project',
+			fieldName: 'locationDescription',
+			url: 'location-description',
+			validators: [
+				new StringValidator({
+					maxLength: { maxLength: 2000 },
+					minLength: { minLength: 2 }
+				})
+			]
+		},
+		singleOrLinear: {
+			type: COMPONENT_TYPES.RADIO,
+			title: 'Single or Linear',
+			pageTitle: 'Single or Linear',
+			question: 'Is the site single or linear?',
+			fieldName: 'singleOrLinear',
+			url: 'single-or-linear',
+			options: [
+				{ text: 'Single', value: 'single' },
+				{ text: 'Linear', value: 'linear' }
+			],
+			validators: [new RequiredValidator()]
+		},
+		singleGridReferences: {
+			type: COMPONENT_TYPES.MULTI_FIELD_INPUT,
+			title: `Single Site Grid References`,
+			pageTitle: `Single Site Grid References`,
+			question: `Enter grid references`,
+			fieldName: `singleSiteGridReferences`,
+			url: `grid-references-single-site`,
+			validators: [
+				new MultiFieldInputValidator({
+					fields: [
+						{
+							fieldName: `easting`,
+							errorMessage: 'Please enter easting grid references',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						},
+						{
+							fieldName: `northing`,
+							errorMessage: 'Please enter northing grid references',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						}
+					]
+				})
+			],
+			inputFields: [
+				{ fieldName: `easting`, label: 'Easting', hint: `For example, 123456` },
+				{ fieldName: `northing`, label: 'Northing', hint: `For example, 345678` }
+			]
+		},
+		linearGridReferences: {
+			type: CUSTOM_COMPONENTS.GROUPED_MULTI_FIELD_INPUT,
+			title: `Linear Site Grid References`,
+			pageTitle: `Linear Site Grid References`,
+			question: `Enter grid references`,
+			fieldName: `linearSiteGridReferences`,
+			url: `grid-references-linear-site`,
+			validators: [
+				new MultiFieldInputValidator({
+					fields: [
+						{
+							fieldName: `startEasting`,
+							errorMessage: 'Please enter a start easting grid reference',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						},
+						{
+							fieldName: `startNorthing`,
+							errorMessage: 'Please enter a start northing grid reference',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						},
+						{
+							fieldName: `middleEasting`,
+							errorMessage: 'Please enter a middle easting grid reference',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						},
+						{
+							fieldName: `middleNorthing`,
+							errorMessage: 'Please enter a middle northing grid reference',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						},
+						{
+							fieldName: `endEasting`,
+							errorMessage: 'Please enter an end easting grid references',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						},
+						{
+							fieldName: `endNorthing`,
+							errorMessage: 'Please enter an end northing grid references',
+							required: true,
+							regex: { regex: /^[0-9]{6}$/, regexMessage: 'Please enter a 6 digit numeric grid reference' }
+						}
+					]
+				})
+			],
+			inputGroups: [
+				{
+					title: 'Start',
+					fields: [
+						{ fieldName: `startEasting`, label: 'Easting' },
+						{ fieldName: `startNorthing`, label: 'Northing' }
+					]
+				},
+				{
+					title: 'Middle',
+					fields: [
+						{ fieldName: `middleEasting`, label: 'Easting' },
+						{ fieldName: `middleNorthing`, label: 'Northing' }
+					]
+				},
+				{
+					title: 'End',
+					fields: [
+						{ fieldName: `endEasting`, label: 'Easting' },
+						{ fieldName: `endNorthing`, label: 'Northing' }
+					]
+				}
 			]
 		}
 	};
--- packages/lib/forms/custom-components/index.ts
+++ packages/lib/forms/custom-components/index.ts
@@ -1,15 +1,18 @@
 import FileUpload from './file-upload/question.js';
 import FullAddress from './full-address/question.js';
 import SelectDocuments from './select-documents/question.js';
+import GroupedMultiFieldInput from './grouped-multi-field-input/question.js';
 
 export const CUSTOM_COMPONENTS = Object.freeze({
 	FILE_UPLOAD: 'file-upload',
 	FULL_ADDRESS: 'full-address',
-	SELECT_DOCUMENTS: 'select-documents'
+	SELECT_DOCUMENTS: 'select-documents',
+	GROUPED_MULTI_FIELD_INPUT: 'grouped-multi-field-input'
 });
 
 export const CUSTOM_COMPONENT_CLASSES = Object.freeze({
 	[CUSTOM_COMPONENTS.FILE_UPLOAD]: FileUpload,
 	[CUSTOM_COMPONENTS.FULL_ADDRESS]: FullAddress,
-	[CUSTOM_COMPONENTS.SELECT_DOCUMENTS]: SelectDocuments
+	[CUSTOM_COMPONENTS.SELECT_DOCUMENTS]: SelectDocuments,
+	[CUSTOM_COMPONENTS.GROUPED_MULTI_FIELD_INPUT]: GroupedMultiFieldInput
 });
