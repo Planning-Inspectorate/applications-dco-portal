e7ae4c611e14e23a33aebb490ef5d7c02a8bd83e
Tom Elliott <tomxelliott@hotmail.co.uk>
2025-12-02 14:43:18 +0000

    feat(portal): add statutory nuisance info journey

---
+++ apps/portal/src/app/views/statutory-nuisance-information/questions.ts
@@ -0,0 +1,37 @@
+// @ts-expect-error - due to not having @types
+import { createQuestions } from '@planning-inspectorate/dynamic-forms/src/questions/create-questions.js';
+// @ts-expect-error - due to not having @types
+import { questionClasses } from '@planning-inspectorate/dynamic-forms/src/questions/questions.js';
+// @ts-expect-error - due to not having @types
+import RequiredValidator from '@planning-inspectorate/dynamic-forms/src/validator/required-validator.js';
+// @ts-expect-error - due to not having @types
+import { COMPONENT_TYPES } from '@planning-inspectorate/dynamic-forms';
+import { CUSTOM_COMPONENT_CLASSES, CUSTOM_COMPONENTS } from '@pins/dco-portal-lib/forms/custom-components/index.ts';
+
+export function getQuestions() {
+	const questions = {
+		hasStatutoryNuisanceStatement: {
+			type: COMPONENT_TYPES.BOOLEAN,
+			title: 'Does the project include a statutory nuisance statement?',
+			question: 'Does the project include a statutory nuisance statement?',
+			fieldName: 'hasStatutoryNuisanceStatement',
+			url: 'has-statutory-nuisance-statement',
+			validators: [new RequiredValidator()]
+		},
+		statutoryNuisanceStatement: {
+			type: CUSTOM_COMPONENTS.SELECT_DOCUMENTS,
+			title: 'Which documents relate to the statutory nuisance statement?',
+			question: 'Which documents relate to the statutory nuisance statement?',
+			fieldName: 'statutoryNuisanceStatement',
+			url: 'statutory-nuisance-statement',
+			validators: [new RequiredValidator()]
+		}
+	};
+
+	const classes = {
+		...questionClasses,
+		...CUSTOM_COMPONENT_CLASSES
+	};
+
+	return createQuestions(questions, classes, {});
+}
---
+++ apps/portal/src/app/views/statutory-nuisance-information/save.test.ts
@@ -0,0 +1,198 @@
+// @ts-nocheck
+
+import { describe, it, mock } from 'node:test';
+import assert from 'node:assert';
+import { buildSaveController } from './save.ts';
+import { mockLogger } from '@pins/dco-portal-lib/testing/mock-logger.ts';
+import { APPLICATION_SECTION_ID } from '../constants.ts';
+
+describe('statutory-nuisance-information save', () => {
+	describe('buildSaveController', () => {
+		it('should save statutory nuisance info journey successfully if hasStatutoryNuisanceStatement is yes', async () => {
+			const mockDb = {
+				$transaction: mock.fn((fn) => fn(mockDb)),
+				case: {
+					findUnique: mock.fn(() => ({
+						id: 'case-id-1'
+					})),
+					update: mock.fn()
+				},
+				supportingEvidence: {
+					deleteMany: mock.fn(),
+					upsert: mock.fn()
+				}
+			};
+			const mockReq = {
+				baseUrl: '/statutory-nuisance-information',
+				session: {
+					caseReference: 'EN123456'
+				}
+			};
+			const mockRes = {
+				redirect: mock.fn(),
+				locals: {
+					journeyResponse: {
+						answers: {
+							statutoryNuisanceStatement: 'doc-id-3,doc-id-2',
+							hasStatutoryNuisanceStatement: 'yes'
+						}
+					}
+				}
+			};
+
+			const controller = buildSaveController(
+				{
+					db: mockDb,
+					logger: mockLogger()
+				},
+				APPLICATION_SECTION_ID.STATUTORY_NUISANCE_INFORMATION
+			);
+			await controller(mockReq, mockRes);
+
+			assert.strictEqual(mockRes.redirect.mock.callCount(), 1);
+			assert.strictEqual(mockRes.redirect.mock.calls[0].arguments[0], '/');
+
+			assert.strictEqual(mockDb.case.findUnique.mock.callCount(), 1);
+			assert.strictEqual(mockDb.case.update.mock.callCount(), 1);
+
+			assert.strictEqual(mockDb.supportingEvidence.deleteMany.mock.callCount(), 1);
+			assert.strictEqual(mockDb.supportingEvidence.upsert.mock.callCount(), 2);
+		});
+		it('should save statutory nuisance info journey successfully if hasStatutoryNuisanceStatement is no', async () => {
+			const mockDb = {
+				$transaction: mock.fn((fn) => fn(mockDb)),
+				case: {
+					findUnique: mock.fn(() => ({
+						id: 'case-id-1'
+					})),
+					update: mock.fn()
+				},
+				supportingEvidence: {
+					deleteMany: mock.fn(),
+					upsert: mock.fn()
+				}
+			};
+			const mockReq = {
+				baseUrl: '/statutory-nuisance-information',
+				session: {
+					caseReference: 'EN123456'
+				}
+			};
+			const mockRes = {
+				redirect: mock.fn(),
+				locals: {
+					journeyResponse: {
+						answers: {
+							hasStatutoryNuisanceStatement: 'no'
+						}
+					}
+				}
+			};
+
+			const controller = buildSaveController(
+				{
+					db: mockDb,
+					logger: mockLogger()
+				},
+				APPLICATION_SECTION_ID.STATUTORY_NUISANCE_INFORMATION
+			);
+			await controller(mockReq, mockRes);
+
+			assert.strictEqual(mockRes.redirect.mock.callCount(), 1);
+			assert.strictEqual(mockRes.redirect.mock.calls[0].arguments[0], '/');
+
+			assert.strictEqual(mockDb.case.findUnique.mock.callCount(), 1);
+			assert.strictEqual(mockDb.case.update.mock.callCount(), 1);
+
+			assert.strictEqual(mockDb.supportingEvidence.deleteMany.mock.callCount(), 1);
+			assert.strictEqual(mockDb.supportingEvidence.upsert.mock.callCount(), 0);
+		});
+		it('should redirect to not found page if the case data is not present in db', async () => {
+			const mockDb = {
+				$transaction: mock.fn((fn) => fn(mockDb)),
+				case: {
+					findUnique: mock.fn()
+				}
+			};
+			const mockReq = {
+				baseUrl: '/statutory-nuisance-information',
+				session: {
+					caseReference: 'EN123456'
+				}
+			};
+			const mockRes = {
+				render: mock.fn(),
+				status: mock.fn(),
+				locals: {
+					journeyResponse: {
+						answers: {
+							hasStatutoryNuisanceStatement: 'no'
+						}
+					}
+				}
+			};
+
+			const controller = buildSaveController(
+				{
+					db: mockDb,
+					logger: mockLogger()
+				},
+				APPLICATION_SECTION_ID.STATUTORY_NUISANCE_INFORMATION
+			);
+			await controller(mockReq, mockRes);
+
+			assert.strictEqual(mockRes.render.mock.callCount(), 1);
+			assert.strictEqual(mockRes.status.mock.callCount(), 1);
+			assert.strictEqual(mockRes.render.mock.calls[0].arguments[0], 'views/layouts/error');
+			assert.deepStrictEqual(mockRes.render.mock.calls[0].arguments[1], {
+				pageTitle: 'Page not found',
+				messages: [
+					'If you typed the web address, check it is correct.',
+					'If you pasted the web address, check you copied the entire address.'
+				]
+			});
+		});
+		it('should throw error is issue encountered during db transaction to save case data', async () => {
+			const mockDb = {
+				$transaction: mock.fn((fn) => fn(mockDb)),
+				case: {
+					findUnique: mock.fn(() => ({
+						id: 'case-id-1'
+					})),
+					update: mock.fn(() => {
+						throw new Prisma.PrismaClientKnownRequestError('Error', { code: 'E1' });
+					})
+				},
+				supportingEvidence: {
+					deleteMany: mock.fn()
+				}
+			};
+			const mockReq = {
+				baseUrl: '/statutory-nuisance-information',
+				session: {
+					caseReference: 'EN123456'
+				}
+			};
+			const mockRes = {
+				locals: {
+					journeyResponse: {
+						answers: {
+							hasStatutoryNuisanceStatement: 'no'
+						}
+					}
+				}
+			};
+
+			const controller = buildSaveController(
+				{
+					db: mockDb,
+					logger: mockLogger()
+				},
+				APPLICATION_SECTION_ID.STATUTORY_NUISANCE_INFORMATION
+			);
+			await assert.rejects(() => controller(mockReq, mockRes), {
+				message: 'error saving statutory nuisance information journey'
+			});
+		});
+	});
+});
