433dc226fdec62f664942d37b63df7e90726844e
james.barnes <james.barnes@solirius.com>
2025-12-05 16:00:31 +0000

    feat: create draft order and explanantory memorandum section

---
+++ apps/portal/src/app/views/draft-order-and-explanatory-memorandum/questions.ts
@@ -0,0 +1,46 @@
+// @ts-expect-error - due to not having @types
+import { createQuestions } from '@planning-inspectorate/dynamic-forms/src/questions/create-questions.js';
+// @ts-expect-error - due to not having @types
+import { questionClasses } from '@planning-inspectorate/dynamic-forms/src/questions/questions.js';
+// @ts-expect-error - due to not having @types
+import RequiredValidator from '@planning-inspectorate/dynamic-forms/src/validator/required-validator.js';
+import { CUSTOM_COMPONENT_CLASSES, CUSTOM_COMPONENTS } from '@pins/dco-portal-lib/forms/custom-components/index.ts';
+
+export function getQuestions() {
+	const questions = {
+		draftDevelopmentConsentOrder: {
+			type: CUSTOM_COMPONENTS.SELECT_DOCUMENTS,
+			title: 'Draft Development Consent Order',
+			pageTitle: 'Draft Development Consent Order',
+			question: 'Which documents relate to the draft development consent order?',
+			fieldName: 'draftDevelopmentConsentOrder',
+			url: 'draft-development-consent-order',
+			validators: [new RequiredValidator()]
+		},
+		siValidationReportSuccessEmail: {
+			type: CUSTOM_COMPONENTS.SELECT_DOCUMENTS,
+			title: 'SI Validation Report Success Email',
+			pageTitle: 'SI Validation Report Success Email',
+			question: 'Which documents relate to the SI validation report success email?',
+			fieldName: 'siValidationReportSuccessEmail',
+			url: 'si-validation-report-success-email',
+			validators: [new RequiredValidator()]
+		},
+		explanatoryMemorandum: {
+			type: CUSTOM_COMPONENTS.SELECT_DOCUMENTS,
+			title: 'Explanatory Memorandum',
+			pageTitle: 'Explanatory Memorandum',
+			question: 'Which documents relate to the Explanatory Memorandum?',
+			fieldName: 'explanatoryMemorandum',
+			url: 'explanatory-memorandum',
+			validators: [new RequiredValidator()]
+		}
+	};
+
+	const classes = {
+		...questionClasses,
+		...CUSTOM_COMPONENT_CLASSES
+	};
+
+	return createQuestions(questions, classes, {});
+}
