016f57bbea46faaacc41f31ba95b697eac297087
james.barnes <james.barnes@solirius.com>
2025-12-16 13:36:34 +0000

    feat: enable multiple document subcategories in a select document question

--- apps/portal/src/app/views/middleware/select-document-middleware.ts
+++ apps/portal/src/app/views/middleware/select-document-middleware.ts
@@ -21,3 +21,25 @@ export function selectDocumentQuestionMiddleware({ db }: PortalService) {
 		next();
 	};
 }
+
+export function selectMultipleDocumentQuestionMiddleware({ db }: PortalService, documentSubCategoryIds: string[]) {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		const { section, question } = req.params;
+		const subCategories = documentSubCategoryIds.filter((id) =>
+			(Object.values(DOCUMENT_SUB_CATEGORY_ID) as string[]).includes(id)
+		);
+		if (subCategories.length) {
+			const { journey } = res.locals;
+
+			const sectionObj = journey.getSection(section);
+			const questionObj = journey.getQuestionBySectionAndName(section, question);
+
+			if (!questionObj || !sectionObj) {
+				return res.redirect(journey.taskListUrl);
+			}
+			const documentOptions = await populateDocumentOptions(req, db, question);
+			questionObj.setOptions(documentOptions);
+		}
+		next();
+	};
+}
