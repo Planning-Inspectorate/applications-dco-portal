d0129c809c6f28bd413769059ca4684a93d74138
james.barnes <james.barnes@solirius.com>
2025-12-18 17:22:28 +0000

    feat: create other consents or licence details section

---
+++ apps/portal/src/app/views/other-consents-or-licenses-details/controller.ts
@@ -0,0 +1,69 @@
+import { PortalService } from '#service';
+import { kebabCaseToCamelCase } from '@pins/dco-portal-lib/util/questions.ts';
+import {
+	DOCUMENT_CATEGORY_STATUS_ID,
+	DOCUMENT_SUB_CATEGORY_ID
+} from '@pins/dco-portal-database/src/seed/data-static.ts';
+import type { AsyncRequestHandler } from '@pins/dco-portal-lib/util/async-handler.ts';
+import type { Request, Response } from 'express';
+import { notFoundHandler } from '@pins/dco-portal-lib/middleware/errors.ts';
+import { getSupportingEvidenceIds } from '../supporting-evidence/util.ts';
+import type { PrismaClient } from '@pins/dco-portal-database/src/client/client.ts';
+
+export function buildOtherConsentsOrLicencesDetailsHomePage(
+	{ db }: PortalService,
+	applicationSectionId: string
+): AsyncRequestHandler {
+	return async (req, res) => {
+		const caseData = await db.case.findUnique({
+			where: { reference: req.session?.caseReference }
+		});
+
+		if (
+			(caseData as any)[`${kebabCaseToCamelCase(applicationSectionId)}StatusId`] !==
+			DOCUMENT_CATEGORY_STATUS_ID.NOT_STARTED
+		) {
+			await populateForm(req, res, db, applicationSectionId);
+		}
+
+		res.redirect(`${req.baseUrl}/details/has-other-consents`);
+	};
+}
+
+async function populateForm(req: Request, res: Response, db: PrismaClient, applicationSectionId: string) {
+	const otherConsentsOrLicencesDocumentIds = [
+		DOCUMENT_SUB_CATEGORY_ID.CONSENTS_AND_LICENCES_REQUIRED_UNDER_OTHER_LEGISLATION
+	];
+
+	const caseData = await db.case.findUnique({
+		where: { reference: req.session?.caseReference },
+		include: {
+			SupportingEvidence: {
+				where: {
+					SubCategory: {
+						id: { in: otherConsentsOrLicencesDocumentIds }
+					}
+				}
+			}
+		}
+	});
+
+	if (!caseData) {
+		return notFoundHandler(req, res);
+	}
+
+	const forms = req.session.forms || (req.session.forms = {});
+	const hasEvidence = (caseData?.SupportingEvidence?.length ?? 0) > 0;
+
+	forms[applicationSectionId] = hasEvidence
+		? {
+				hasOtherConsents: 'yes',
+				otherConsentsDocuments: getSupportingEvidenceIds(
+					caseData.SupportingEvidence,
+					DOCUMENT_SUB_CATEGORY_ID.CONSENTS_AND_LICENCES_REQUIRED_UNDER_OTHER_LEGISLATION
+				)
+			}
+		: {
+				hasOtherConsents: 'no'
+			};
+}
---
+++ apps/portal/src/app/views/other-consents-or-licenses-details/questions.ts
@@ -0,0 +1,40 @@
+// @ts-expect-error - due to not having @types
+import { createQuestions } from '@planning-inspectorate/dynamic-forms/src/questions/create-questions.js';
+// @ts-expect-error - due to not having @types
+import { questionClasses } from '@planning-inspectorate/dynamic-forms/src/questions/questions.js';
+// @ts-expect-error - due to not having @types
+import RequiredValidator from '@planning-inspectorate/dynamic-forms/src/validator/required-validator.js';
+// @ts-expect-error - due to not having @types
+import { COMPONENT_TYPES } from '@planning-inspectorate/dynamic-forms';
+import { CUSTOM_COMPONENT_CLASSES, CUSTOM_COMPONENTS } from '@pins/dco-portal-lib/forms/custom-components/index.ts';
+import { DOCUMENT_SUB_CATEGORY_ID } from '@pins/dco-portal-database/src/seed/data-static.ts';
+
+export function getQuestions() {
+	const questions = {
+		hasOtherConsents: {
+			type: COMPONENT_TYPES.BOOLEAN,
+			title: 'Has Other Consents or Licences',
+			pageTitle: 'Has Other Consents Or Licences',
+			question: 'Does the project require other consents or licences?',
+			fieldName: 'hasOtherConsents',
+			url: 'has-other-consents',
+			validators: [new RequiredValidator()]
+		},
+		otherConsentsDocuments: {
+			type: CUSTOM_COMPONENTS.SELECT_DOCUMENTS,
+			title: 'Other Consents or Licences Documents',
+			pageTitle: 'Other Consents or Licences Documents',
+			question: 'Which documents identify the other consents or licences?',
+			fieldName: 'otherConsentsDocuments',
+			url: DOCUMENT_SUB_CATEGORY_ID.CONSENTS_AND_LICENCES_REQUIRED_UNDER_OTHER_LEGISLATION,
+			validators: [new RequiredValidator()]
+		}
+	};
+
+	const classes = {
+		...questionClasses,
+		...CUSTOM_COMPONENT_CLASSES
+	};
+
+	return createQuestions(questions, classes, {});
+}
