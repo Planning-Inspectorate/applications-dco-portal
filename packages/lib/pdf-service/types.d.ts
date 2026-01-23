1d25a52e95d676cd57f1c4c0d4bdcb54c11a5305
james.barnes <james.barnes@solirius.com>
2026-01-23 17:02:41 +0000

    feat: mapper function skeleton

--- packages/lib/pdf-service/types.d.ts
+++ packages/lib/pdf-service/types.d.ts
@@ -1,3 +1,28 @@
+import { Prisma } from '@pins/dco-portal-database/src/client/client.ts';
+
+export type FullCase = Prisma.CaseGetPayload<{
+	include: {
+		SupportingEvidence: true;
+		ApplicantDetails: {
+			include: { Address: true };
+		};
+		AgentDetails: {
+			include: { Address: true };
+		};
+		CasePaymentMethod: true;
+		ProjectSingleSite: true;
+		ProjectLinearSite: true;
+		NonOffshoreGeneratingStation: true;
+		OffshoreGeneratingStation: true;
+		HighwayRelatedDevelopment: true;
+		RailwayDevelopment: true;
+		HarbourFacilities: true;
+		Pipelines: true;
+		HazardousWasteFacility: true;
+		DamOrReservoir: true;
+	};
+}>;
+
 export interface pdfServiceConfig {
 	url: string;
 }
