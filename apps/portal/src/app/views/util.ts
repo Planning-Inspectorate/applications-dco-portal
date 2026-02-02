10e9b999f0a716c9e1e36252bcbfbd256bd5914c
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-02-02 17:37:36 +0000

    feat(portal): update logic and content around virus scans on document upload landing page
    feat(portal): remove landing page function param
    
    feat(portal): reuse same header variable for both tables

--- apps/portal/src/app/views/util.ts
+++ apps/portal/src/app/views/util.ts
@@ -1,13 +1,5 @@
 import { DOCUMENT_CATEGORY, DOCUMENT_CATEGORY_STATUS_ID } from '@pins/dco-portal-database/src/seed/data-static.ts';
 import { APPLICATION_SECTION } from './constants.ts';
-import { kebabCaseToCamelCase } from '@pins/dco-portal-lib/util/questions.ts';
-import type { PortalService } from '#service';
-import type { AsyncRequestHandler } from '@pins/dco-portal-lib/util/async-handler.ts';
-
-// @ts-expect-error - due to not having @types
-import { expressValidationErrorsToGovUkErrorList } from '@planning-inspectorate/dynamic-forms/src/validator/validation-error-handler.js';
-// @ts-expect-error - due to not having @types
-import { BOOLEAN_OPTIONS } from '@planning-inspectorate/dynamic-forms/src/components/boolean/question.js';
 
 export function getDocumentCategoryDisplayName(documentCategoryId: string): string {
 	return DOCUMENT_CATEGORY.find(({ id }) => id === documentCategoryId)?.displayName ?? '';
@@ -23,44 +15,6 @@ export function statusIdRadioButtonValue(statusId: string): string {
 		[DOCUMENT_CATEGORY_STATUS_ID.IN_PROGRESS]: 'no'
 	};
 	return STATUS_ID_TO_RADIO_VALUE[statusId] ?? '';
-}
-
-export function buildIsTaskCompleted(
-	service: PortalService,
-	journeyId: string,
-	buildHomepageFunc: ({ db }: PortalService, applicationSectionId: string, viewData?: object) => AsyncRequestHandler
-): AsyncRequestHandler {
-	return async (req, res) => {
-		const { db } = service;
-
-		const sectionCompletedFieldName = `${kebabCaseToCamelCase(journeyId)}IsCompleted`;
-		const isSectionCompleted = req.body[sectionCompletedFieldName];
-
-		if (!isSectionCompleted) {
-			req.body.errors = {
-				[sectionCompletedFieldName]: { msg: 'You must select an answer' }
-			};
-			req.body.errorSummary = expressValidationErrorsToGovUkErrorList(req.body.errors);
-
-			const taskHomepage = buildHomepageFunc(service, journeyId, {
-				errors: req.body.errors,
-				errorSummary: req.body.errorSummary
-			});
-			return taskHomepage(req, res);
-		}
-
-		const statusId: string =
-			isSectionCompleted === BOOLEAN_OPTIONS.YES
-				? DOCUMENT_CATEGORY_STATUS_ID.COMPLETED
-				: DOCUMENT_CATEGORY_STATUS_ID.IN_PROGRESS;
-
-		await db.case.update({
-			where: { reference: req.session.caseReference },
-			data: { [`${kebabCaseToCamelCase(journeyId)}StatusId`]: statusId }
-		});
-
-		res.redirect('/');
-	};
 }
 
 export function populateMultiSubcategoryCheckboxes(subCategories: { count: number; id: string }[]) {
