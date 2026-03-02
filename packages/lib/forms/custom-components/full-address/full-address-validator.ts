e48f4536d0dddb27a39857a5cbc3166b3fa19b30
Tom Elliott <tomxelliott@hotmail.co.uk>
2026-03-02 16:38:09 +0000

    fix(portal): ensure validation correctly runs on app/agent addresses

--- packages/lib/forms/custom-components/full-address/full-address-validator.ts
+++ packages/lib/forms/custom-components/full-address/full-address-validator.ts
@@ -1,10 +1,23 @@
 import { body } from 'express-validator';
 import AddressValidator, {
+	addressLine1MaxLength,
+	addressLine1MinLength,
+	addressLine2MaxLength,
+	addressLine2MinLength,
+	townCityMaxLength,
+	townCityMinLength,
+	countyMaxLength,
+	countyMinLength,
+	postcodeMaxLength,
+	postcodeMinLength,
 	validatePostcode
 	// @ts-expect-error - due to not having @types
 } from '@planning-inspectorate/dynamic-forms/src/validator/address-validator.js';
 import type { AddressValidatorOpts } from './types.d.ts';
 import FullAddressQuestion from './question.js';
+
+export const countryMaxLength = 250;
+export const countryMinLength = 0;
 
 export default class FullAddressValidator extends AddressValidator {
 	declare requiredFields?: { [key: string]: boolean };
@@ -35,9 +48,9 @@ export default class FullAddressValidator extends AddressValidator {
 		}
 
 		return validator
-			.isLength({ min: super.addressLine1MinLength, max: super.addressLine1MaxLength })
+			.isLength({ min: addressLine1MinLength, max: addressLine1MaxLength })
 			.bail()
-			.withMessage(`${fieldLabel} must be ${super.addressLine1MaxLength} characters or less`);
+			.withMessage(`${fieldLabel} must be ${addressLine1MaxLength} characters or less`);
 	}
 
 	#addressLine2Rule(fieldName: string, fieldLabel: string) {
@@ -49,9 +62,9 @@ export default class FullAddressValidator extends AddressValidator {
 		}
 
 		return validator
-			.isLength({ min: super.addressLine2MinLength, max: super.addressLine2MaxLength })
+			.isLength({ min: addressLine2MinLength, max: addressLine2MaxLength })
 			.bail()
-			.withMessage(`${fieldLabel} must be ${super.addressLine2MaxLength} characters or less`);
+			.withMessage(`${fieldLabel} must be ${addressLine2MaxLength} characters or less`);
 	}
 
 	#townCityRule(fieldName: string, fieldLabel: string) {
@@ -62,9 +75,9 @@ export default class FullAddressValidator extends AddressValidator {
 			validator.notEmpty().withMessage(`Enter ${fieldLabel.toLocaleLowerCase()}`);
 		}
 		return validator
-			.isLength({ min: super.townCityMinLength, max: super.townCityMaxLength })
+			.isLength({ min: townCityMinLength, max: townCityMaxLength })
 			.bail()
-			.withMessage(`${fieldLabel} must be ${super.townCityMaxLength} characters or less`);
+			.withMessage(`${fieldLabel} must be ${townCityMaxLength} characters or less`);
 	}
 
 	#countyRule(fieldName: string, fieldLabel: string) {
@@ -76,9 +89,9 @@ export default class FullAddressValidator extends AddressValidator {
 		}
 
 		return validator
-			.isLength({ min: super.countyMinLength, max: super.countyMaxLength })
+			.isLength({ min: countyMinLength, max: countyMaxLength })
 			.bail()
-			.withMessage(`${fieldLabel} must be ${super.countyMaxLength} characters or less`);
+			.withMessage(`${fieldLabel} must be ${countyMaxLength} characters or less`);
 	}
 
 	#postCodeRule(fieldName: string, fieldLabel: string) {
@@ -90,7 +103,7 @@ export default class FullAddressValidator extends AddressValidator {
 		}
 
 		return validator
-			.isLength({ min: super.postcodeMinLength, max: super.postcodeMaxLength })
+			.isLength({ min: postcodeMinLength, max: postcodeMaxLength })
 			.bail()
 			.withMessage(`Enter a valid ${fieldLabel.toLocaleLowerCase()}`)
 			.custom((postcode) => {
@@ -107,8 +120,8 @@ export default class FullAddressValidator extends AddressValidator {
 		}
 
 		return validator
-			.isLength({ min: super.countryMinLength, max: super.countryMaxLength })
+			.isLength({ min: countryMinLength, max: countryMaxLength })
 			.bail()
-			.withMessage(`${fieldLabel} must be ${super.countryMaxLength} characters or less`);
+			.withMessage(`${fieldLabel} must be ${countryMaxLength} characters or less`);
 	}
 }
