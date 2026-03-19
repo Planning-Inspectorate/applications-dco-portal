b414076548c3eacd1db345237674669307a541c7
james.barnes <james.barnes@solirius.com>
2026-03-19 14:29:21 +0000

    refactor: make pdf browser a singleton class

--- apps/pdf-service-api/src/app/lib/generate-pdf.ts
+++ apps/pdf-service-api/src/app/lib/generate-pdf.ts
@@ -1,26 +1,17 @@
-import type { Browser } from 'puppeteer-core';
+import type { Page } from 'puppeteer-core';
 
-export const generatePdf = async (browser: Browser, html: string): Promise<Uint8Array> => {
-	try {
-		const page = await browser.newPage();
+export const generatePdf = async (html: string, page: Page): Promise<Uint8Array> => {
+	await page.setContent(html, {
+		waitUntil: 'domcontentloaded'
+	});
 
-		await page.setContent(html, {
-			waitUntil: 'domcontentloaded'
-		});
+	await page.emulateMediaType('print');
 
-		await page.emulateMediaType('print');
+	const pdfBuffer = await page.pdf({
+		format: 'A4',
+		scale: 0.9,
+		printBackground: true
+	});
 
-		const pdfBuffer = await page.pdf({
-			format: 'A4',
-			scale: 0.9,
-			printBackground: true
-		});
-
-		await browser.close();
-
-		return Buffer.from(pdfBuffer);
-	} catch (err) {
-		const errorMessage = err instanceof Error ? err.message : '';
-		throw new Error(errorMessage);
-	}
+	return Buffer.from(pdfBuffer);
 };
