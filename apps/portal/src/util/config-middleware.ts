c24966cd4d2c0a62624837ef8c01aa77726ae10f
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-08-25 15:51:35 +0100

    chore: fix stylelint errors

--- apps/portal/src/util/config-middleware.ts
+++ apps/portal/src/util/config-middleware.ts
@@ -6,7 +6,7 @@ import type { Handler } from 'express';
 export function addLocalsConfiguration(): Handler {
 	return (req, res, next) => {
 		res.locals.config = {
-			styleFile: 'style-647358e7.css',
+			styleFile: 'style-f766a7d3.css',
 			cspNonce: res.locals.cspNonce,
 			headerTitle: 'Apply for and manage your infrastructure project',
 			serviceUrl: '/',
